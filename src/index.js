de3ce75c0ecaad64dcdad6a86f39442919596bcb
onigirinorio <50672715+onigirinorio@users.noreply.github.com>
2022-01-29 17:55:44 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -146,7 +146,7 @@
 /**
  * mapやfilterを使った配列の処理
  */
-const nameArr = ["田中", "山田", "はい"];
+// const nameArr = ["田中", "山田", "はい"];
 // for (let index = 0; index < nameArr.length; index++) {
 //   console.log(`${index + 1}番目は${nameArr[index]}です`);
 // }
@@ -164,11 +164,32 @@ const nameArr = ["田中", "山田", "はい"];
 // });
 // console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "はい") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "はい") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+//　ある条件　？　条件がtrueの時　：　条件がfalseの時
+// const val1 = 1 > 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = 1300;
+// // toLocaleString：４桁以上の数字に勝手にコンマをつけてくれる関数
+// // console.log(num.toLocaleString());
+
+// // typeof：その変数の方を調べてくれる関数
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+};
+console.log(checkSum(50, 40));
